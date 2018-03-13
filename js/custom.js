556f09f3bd01f1405ca3e71c01082162a66906be
David Haynes <DavidHaynesVictoria@gmail.com>
2018-03-13 07:31:52 -0700

    adding portfolio file to github repo for webpage

---
+++ js/custom.js
@@ -0,0 +1,99 @@
+/* global  $,  jquery, console, alert*/
+$(function(){
+  "use strict";
+  var rNav           = $(".right-nav");
+  var  hInfo          = $(".home .info");
+  var  home           = $(".home");
+  var  navBtn         = $(".slide-nav-btn");
+  var  navBtnActive   = "slide-nav-btn-active";
+  var  nav            = $(".slide-nav");
+  var  navActive      = "slide-nav-active";
+  var  rNavSpan       = $('.right-nav ul li i');
+  var  up             = $('.up i');
+  var  body           = $("html, body");
+
+  //-------------- Right-Nav [position]--------------//
+  rNav.css("top", (home.height() - rNav.height()) / 2);
+  //-------------- Home-info [position]--------------//
+  hInfo.css("top", (home.height() - hInfo.height()) / 2);
+
+
+  //------- start slide-nav [btn action and style]-----//
+  navBtn.on('click', function(){
+    $(this).toggleClass(navBtnActive);
+    nav.toggleClass(navActive);
+  });
+
+  $(document).on('scroll', function() {
+    var y = $(this).scrollTop();
+    if (y > 200) {
+      rNavSpan.css("background", "rgba(0, 0, 0, 0.54)");
+      navBtn.css("background", "rgba(0, 0, 0, 0.54)");
+      nav.css("background", "rgba(0, 0, 0, 0.8)");
+    } else {
+      rNavSpan.css("background", "rgba(255, 255, 255, 0.05)");
+      navBtn.css("background", "rgba(255, 255, 255, 0.05)");
+      nav.css("background", "rgba(0, 0, 0, 0.38)");
+    }
+  });
+  //------------ [end slide-nav-btnt]---------//
+
+  //------- materialize carousel -------------//
+  $('.carousel').carousel();
+  //----------- modal [services-section] -----//
+  $('.modal').modal();
+  body.on('click', function(){
+    $(this).css("overflow", "auto");
+  });
+  //------- arrow up to site -------------//
+  up.on('click', function() {
+    body.animate({
+      scrollTop: -690
+    }, 1500);
+  });
+  // up to page fadeIn fadeOut
+  $(document).on('scroll', function() {
+    var y = $(this).scrollTop();
+    if (y > 800) {
+      up.css("right", "2%");
+    } else {
+      up.css("right", "-200px");
+    }
+  });
+  /*--------------[blog-carousel]-----------*/
+  $('.blog .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 15,
+    responsive: {
+      0: {items: 1},
+      600: {items: 2},
+      1000: {items: 3}
+    }});
+
+
+  // [smoothScroll]
+  $('a').smoothScroll();
+
+});
+
+// -------on load actions [preloader - write effect - body scroll]-----//
+$(window).on('load', function(){
+  $('.preloader').fadeOut(1000);
+  $("body").css("overflow-y", "auto");
+  $(".slide-nav-btn").css("right", "2%");
+  //------- [start type.js script]-----//
+  $(".write").typed({
+    strings: ["Hey, My Name Is David Haynes ",
+      "Web Designer And <br> Front End Developer",
+      "I Live In Vancouver BC",
+      "This Is My Personal Site",
+      "Don't Hesitate To <br> Contact Me"
+    ],
+
+    typeSpeed: 100,
+    loop: true
+  });
+  $(".typed-cursor").hide();
+  // -------[end type.js script] ------ //
+});
